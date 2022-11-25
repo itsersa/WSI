860c112a724d8f87c38d5c1915dafee5ac6fb784
zidenalcena <alcenaziden@gmail.com>
2022-11-25 10:57:41 +0700

    luxbeauteDB

--- TA SEM 3/luxbeauteDB.sql
+++ TA SEM 3/luxbeauteDB.sql
@@ -5,3 +5,9 @@ CREATE TABLE `admins` (
   `role` ENUM('owner','admin') NOT NULL
 )
 
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+)
