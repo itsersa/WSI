b75c7b4363d4b9825caea7f219abd69a8fdb2cf4
zidenalcena <alcenaziden@gmail.com>
2022-12-05 16:45:00 +0700

    admin fitur

--- TA SEM 3/setting.php
+++ TA SEM 3/setting.php
@@ -36,7 +36,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="setting.php">Settings</a></li>
+                        <li><a class="dropdown-item" href="">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                         <li>
                             <hr class="dropdown-divider" />
@@ -108,10 +108,10 @@ if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
                                 <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                                 Tables
                             </a>
-                            
+
                         </div>
                     </div>
-                    
+
                     <!-- MENAMPILKAN SESSION USERNAME -->
                     <div class="sb-sidenav-footer">
                         <?php if ($_SESSION['username']) { ?>
@@ -124,101 +124,50 @@ if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Edit Profile</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Dashboard</li>
+                            <li class="breadcrumb-item active">Edit Profile Here !</li>
                         </ol>
                         <div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
+                            <div class="col-xl-3 col-md-1">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="assets-admin/img/owner.png" class="card-img-top" alt="admin logo">
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
+                            <div class="form-group col-md-3">
+                                <div>
+                                    <label for="inputNama">Nama :</label>
+                                    <input type="text" class="form-control" id="inputNama" value="<?= $_SESSION['nama'] ?>" readonly>
+                                </div>
+                                <div>
+                                    <label for="inputUsername">Username :</label>
+                                    <input type="text" class="form-control" id="inputUsername" value="<?= $_SESSION['username'] ?>">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="inputPicture">Picture :</label>
+                                    <input type="file" class="form-control" id="inputPicture" value="">
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-outline-success" type="button">Update</button>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-area me-1"></i>
-                                        Area Chart Example
-                                    </div>
-                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-bar me-1"></i>
-                                        Bar Chart Example
-                                    </div>
-                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                DataTable Example
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot>
 
-                                </table>
+                            <div class="form-group col-md-3">
+                                <div>
+                                    <label for="inputNotelp">No.Telp</label>
+                                    <input type="text" class="form-control" id="inputNotelp" value="<?= $_SESSION['notelp'] ?>">
+                                </div>
+                                <div>
+                                    <label for="inputPassword">Password</label>
+                                    <input type="text" class="form-control" id="inputPassword" value="<?= $_SESSION['password'] ?>">
+                                </div>
                             </div>
                         </div>
+
                     </div>
                 </main>
+
+
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
