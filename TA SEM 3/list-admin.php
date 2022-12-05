015f7fc3b3931e57b772b2ffd06a119d6fa99700
zidenalcena <alcenaziden@gmail.com>
2022-12-06 00:35:42 +0700

    fitur admin

---
+++ TA SEM 3/list-admin.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <body class="sb-nav-fixed">
+        <!-- PARTIAL KERANGKA DASHBOARD -->
+        <?= include('./partials/rangka.php') ?>
+
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Daftar Admin</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item active">See Admin Here !</li>
+                    </ol>
+                    <div class="row">
+
+                    </div>
+                </div>
+            </main>
+        </div>
+
+        <!-- PARTIAL KERANGKA FOOTER -->
+        <?= include('./partials/foo.php') ?>
+    </body>
+
+    </html>
+
+<?php } else {
+    header("Location: admin.php");
+} ?>
