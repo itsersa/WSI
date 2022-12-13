04a78aed9085a32a224ec6acad70d657e44d18de
zidenalcena <alcenaziden@gmail.com>
2022-12-13 18:39:07 +0700

    fitur admin

--- TA SEM 3/add-admin.php
+++ TA SEM 3/add-admin.php
@@ -11,21 +11,72 @@ if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
         <?= include('./partials/rangka.php') ?>
 
         <div id="layoutSidenav_content">
-            <main>
-                <div class="container-fluid px-4">
-                    <h1 class="mt-4">Tambah Admin</h1>
-                    <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item active">Add Admin Here !</li>
-                    </ol>
-                    <div class="row">
+            <form action="route/add_admin.php" method="post">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Tambah Admin</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Add Admin Here !</li>
+                        </ol>
+                        <?php if (isset($_GET['success'])) { ?>
+                            <div class="alert alert-success" role="alert">
+                                <?= $_GET['success'] ?>
+                            </div>
+                        <?php } ?>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-1">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="assets-admin/img/owner.png" class="card-img-top" alt="admin logo">
+                                </div>
+                            </div>
+
+                            <!-- MENAMPILKAN SESSION DATA ADMIN -->
+                            <div class="form-group col-md-3">
+                                <div>
+                                    <label for="inputNama">Nama :</label>
+                                    <input type="text" class="form-control" id="inputNama" name="nama">
+                                </div>
+                                <div>
+                                    <label for="inputUsername">Username :</label>
+                                    <input type="text" class="form-control" id="inputUsername" name="username">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="inputPicture">Picture :</label>
+                                    <input type="file" class="form-control" id="inputPicture" name="">
+                                </div>
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-outline-success" type="submit" name="submit">Update</button>
+                                </div>
+                            </div>
+
+                            <!-- MENAMPILKAN SESSION DATA ADMIN -->
+                            <div class="form-group col-md-3">
+                                <div>
+                                    <label for="inputNotelp">No.Telp</label>
+                                    <input type="text" class="form-control" id="inputNotelp" name="notelp">
+                                </div>
+                                <div>
+                                    <label for="inputPassword">Password</label>
+                                    <input type="text" class="form-control" id="inputPassword" name="password">
+                                </div>
+                                <div>
+                                    <label for="inputJabatan">Jabatan</label>
+                                    <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="jabatan">
+                                        <option selected>Pilih Jabatan</option>
+                                        <option id="owner" name="jabatan" value="owner">Owner</option>
+                                        <option id="admin" name="jabatan" value="admin">Admin</option>
+                                    </select>
+                                    <!-- <input type="text" class="form-control" id="inputJabatan" name="jabatan"> -->
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
-                </div>
-            </main>
+                </main>
+            </form>
 
             <!-- PARTIAL KERANGKA FOOTER -->
             <?= include('./partials/foo.php') ?>
-        </div>
         </div>
     </body>
 
