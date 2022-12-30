8f8ee107fb14567e65388ffa9f460ba5574a80e1
ersandra <ersandrafairuz@gmail.com>
2022-12-30 21:55:45 +0700

    ubah ke php

---
+++ TA SEM 3/about.php
@@ -0,0 +1,153 @@
+<?php
+>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel="stylesheet" href="style/about.css">
+    <script src="https://kit.fontawesome.com/3dba02c49c.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/cb08e330cf.js"></script>
+</head>
+<body>
+    <div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+					<h1>About Us</h1>
+				</div>
+				<div class="content">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+					<div class="button">
+						<a href="">Belanja di sini!</a>
+					</div>
+				</div>
+			</div>
+			<div class="image-section">
+				<img src="img/melobanner (1).png">
+			</div>
+		</div>
+	</div>
+
+    <div class="container1">
+        <h2>Melody Beauty Location</h2>
+        <center>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.3509817971963!2d113.72291221425509!3d-8.16734969412141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695b2b9892feb%3A0x2438e3891da45d78!2sMelody%20Beauty!5e0!3m2!1sen!2sid!4v1670245874752!5m2!1sen!2sid"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </center>
+    </div>
+
+    <div class="testimonials">
+        <div class="inner">
+          <h3>REVIEWS</h3>
+          <div class="border"></div>
+  
+          <div class="row">
+            <div class="col">
+              <div class="testimonial">
+                <a href="https://shopee.co.id/buyer/8619270/rating"><img src="img/noprofile.png" alt=""></a>
+                <div class="name">oktria123</div>
+                <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </div>
+  
+                <p>
+                  Barang sudah datang dengan selamat dan dapat bonus totebag. Lain kali order sini lagi deh, siapa tau dapat bonus yang lebih menarik lagi. Terima kasih.
+                </p>
+              </div>
+            </div>
+  
+            <div class="col">
+              <div class="testimonial">
+                <a href="https://shopee.co.id/buyer/8619270/rating"><img src="img/noprofile.png" alt=""></a>
+                <div class="name">dwmei_a</div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+  
+                <p>
+                  Tekstur oke, performa bagus, cocok untuk semua jenis kulit. Makasih seller semoga produknya ori ada scan barcokdenya muncul semoga beneran ori. Next order lagi
+                </p>
+              </div>
+            </div>
+  
+            <div class="col">
+              <div class="testimonial">
+                <a href="https://shopee.co.id/buyer/8619270/rating"><img src="img/noprofile.png" alt=""></a>
+                <div class="name">dellaartha17</div>
+                <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                </div>
+  
+                <p>
+                  Efektivitas efektiv banget, kualitas bagus, harga terjangkau. Pengiriman cepat, packing rapi, harga terjangkau.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <footer class="text-muted py-5">
+        <div class="row1">
+            <div class="col">
+                <img src="img/Melody.png" class="logo">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="col">
+                <h3>Shop <div class="underline"><span></span></div></h3>
+                <a href="https://goo.gl/maps/pPdaqyPSxmAdqzAB9"><i class="fa-solid fa-location-dot"></i></a>
+                <p>Ruko Mastrip Square Blok C, Jember</p>
+                <p class="email-id">.com</p>
+                <h4>+123 456789</h4>
+            </div>
+            <div class="col">
+                <h3>Links <div class="underline"><span></span></h3>
+                <ul>
+                    <li><a href="index2.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="konsultasi.html">Konsultasi</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Newsletter <div class="underline"><span></span></h3>
+                <form>
+                    <i class="fa-regular fa-envelope"></i>
+                    <input type="email" placeholder="Enter your email id" required>
+                    <button type="submit"><i class="fa-solid fa-arrow-right"></i></button>
+                </form>
+                <div class="social-icons">
+                    <a href="https://www.instagram.com/melodybeauty29/"><i class="fa-brands fa-instagram"></i></a>
+                    <i class="fa-brands fa-tiktok"></i>
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-whatsapp"></i>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <p class="copyright">Melody Beauty @ 2022 - All Rights Reserved</p> <br>
+     </footer>
+</body>
+</html>
